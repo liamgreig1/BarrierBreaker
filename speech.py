816edd3335b1f7968fc165d794e3b3bd5cccb57d
liamgreig1 <liam.greig1998@googlemail.com>
2019-02-18 10:02:23 +0000

    Add files via upload
    Added Comments

--- speech.py
+++ speech.py
@@ -4,18 +4,26 @@ import time
 from gtts import gTTS
 import os
 
+#key for API
 aai = assemblyai.Client(token='bbc8f48274684b24944a9ed0ec6056b7')
 
+#Temp Text file
 transcript = aai.transcribe(filename='example.txt')
-transcript = aai.transcribe(filename='Intro.mp3')
+#Audio File to be translated 
+transcript = aai.transcribe(filename='Mum.mp3')
 
+#takes audio from mp3 file and converts to text 
 while transcript.status != 'completed':
     transcript = transcript.get()
 
+#adds text to text variable
 text = transcript.text
+
+#prints the text
 print("English: " + text)
 print("\n")
 
+#To Translate text from english to other language
 def tran( toTran, lang ):
 	translator= Translator(From_lang="autodetect", to_lang=lang)
 	translation = translator.translate(toTran)
@@ -32,13 +40,16 @@ tran(text, "pt")
 tran(text, "de")
 
 
-def backTran( fName, lang ):
-	fiName = "Translated/" + fName + ".txt"
-	f = open("Translated/German.txt", "r")
 
-	tts = gTTS(text=f.read(), lang='de')
-	tts.save("german.mp3")
-	os.system("german.mp3")
+#def backTran( fName, lang ):
+#fiName = "Translated/" + fName + ".txt"
+#calls text file to be read out by the computer 
+fiName = "Translated/de.txt"
+f = open(fiName, "r")
+
+tts = gTTS(text=f.read(), lang='de')
+tts.save("german.mp3")
+os.system("german.mp3")
 
 
 # translator= Translator(From_lang="autodetect", to_lang="Chinese")
